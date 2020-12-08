9000866e4ba055f16651cef3b9cde06ad1dbb419
Charlie <peabody00@gmail.com>
2020-12-07 20:15:37 -0600

    commit tree c3fb71fb4b767d4913293993d325cc8d5643175f

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,1 +1,29 @@
+class Genre
+    attr_accessor :name
 
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        save
+    end
+
+    def save
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def songs
+        Song.all.select {|song| song.genre == self}
+    end
+
+    def artists
+        songs.map do |song|
+            song.artist
+        end
+    end
+    
+end
